a077376b3e99ab51c73ba02090259bccecb71f63
afisman <afisman@gmail.com>
2022-07-18 11:17:43 +0200

    Merging issues

--- models/Rating.model.js
+++ models/Rating.model.js
@@ -2,28 +2,28 @@ const { Schema, model } = require("mongoose");
 
 const ratingSchema = new Schema(
   {
-        score: {
-            type: Number,
-            min: 1,
-            max: 5,
-            required: [true, 'Please include a number between 1 and 5'],
-            integer: true
-        },
+    score: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please include a number between 1 and 5'],
+      integer: true
+    },
 
-        comment: {
-            type: String,
-            maxLength: [400, 'The maximum length of reviews is 300 characters'],
-        },
+    comment: {
+      type: String,
+      maxLength: [400, 'The maximum length of reviews is 300 characters'],
+    },
 
-        ratingReceiver: {
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-        },
+    ratingReceiver: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
 
-        ratingGiver: {
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-        },
+    ratingGiver: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
   },
   {
     timestamps: true,
