19da37cae5d004205d5f4918a072e5bb1fcd07dc
afisman <afisman@gmail.com>
2022-07-26 18:17:02 +0200

    updated filter in routes

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -13,4 +13,16 @@ router.get('/:continent', (req, res, next) => {
         .catch(err => res.status(500).json(err))
 })
 
+router.get('/filter', (req, res, next) => {
+    const { from_to } = req.query
+
+    Project
+        .find({ 'projectName': new RegExp(from_to, 'i') })
+        .then(response => {
+            console.log(response)
+            res.json(response)
+        })
+        .catch(err => res.status(500).json(err))
+})
+
 module.exports = router
